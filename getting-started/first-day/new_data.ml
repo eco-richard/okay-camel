c053e289ae04481e0f995b71fcff5141377f41f3
Richard <richard.sd0007@gmail.com>
2023-10-05 18:03:55 -0400

    continue getting started

--- getting-started/first-day/new_data.ml
+++ getting-started/first-day/new_data.ml
@@ -5,3 +5,16 @@ let color_list = [ Red; Blue; Red ]
 (* Data type for a generic binary tree*)
 
 type 'a tree = Leaf | Node of 'a tree * 'a * 'a tree
+
+let t =
+  Node (Node (Leaf, 1, Leaf), 2, Node (Node (Leaf, 3, Leaf), 4, Leaf));
+
+let rec total t =
+  match t with 
+    | Leaf -> 0
+    | Node (l, x, r) -> total l + x + total r;;
+
+let rec flip t = 
+  match t with
+  | Leaf -> Leaf
+    | Node (l, x, r) -> Node (flip r, x flip l);;
